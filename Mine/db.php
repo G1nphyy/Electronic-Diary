72019de2e498dcb3e757c12c8f131e38f09a5e69
G1nphy <161763449+G1nphyy@users.noreply.github.com>
2024-09-21 23:49:38 +0200

    Delete Mine directory

--- Mine/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $server_name = "localhost";
-    $user_name = "root";
-    $password = '';
-    $database = 'mine_db';
-    
--- Mine/toggle_session.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-// Toggle 'show' session variable
-$_SESSION['show'] = isset($_SESSION['show']) ? !$_SESSION['show'] : true;
-
-// Return JSON response
-header('Content-Type: application/json');
-echo json_encode(['success' => true]);
